930ce186ab50fb2b55df2b674c7db0f694fd9825
Prasad Arote <117518329+Sahyadri-1674@users.noreply.github.com>
2023-06-27 14:09:39 +0530

    final code
    mern app deployment

---
+++ routes/blogRoutes.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const { getAllBlogsController, createBlogController, updateBlogController, getBlogByIdController, deleteBlogController,userBlogController } = require('../controller/blogController')
+
+//router obj
+const router = express.Router()
+
+//routes
+//GET || ALL BLOGS
+router.get('/all-blog',getAllBlogsController)
+
+//POST || CREATE BLOG
+router.post('/create-blog',createBlogController)
+
+//PUT || UPDATE BLOG
+router.put('/update-blog/:id',updateBlogController)
+
+
+//GET || SINGLE BLOG DETAILS
+router.get('/get-blog/:id',getBlogByIdController)
+
+//DELETE || DELETE BLOG
+router.delete('/delete-blog/:id',deleteBlogController)
+
+//GET \\ USER BLOG
+router.get('/user-blog/:id',userBlogController)
+module.exports = router
